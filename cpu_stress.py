4d5c5a08a7fed2c056689e74852c853f4a696234
Talha-Altair <cooltalha27@gmail.com>
2021-11-27 21:41:34 +0530

    Updates for 2021-11-27 21:41:34

--- cpu_stress.py
+++ cpu_stress.py
@@ -3,3 +3,7 @@ from cpu_load_generator import load_all_cores
 def stress(load=0.2):
 
     load_all_cores(target_load=load) # generates load on all cores
+
+if __name__ == '__main__':
+
+    stress()
